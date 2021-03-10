c2da5344ff4447f2449a6babbd3077be8637202b
Greg Grinnell <g.grinnell@northeastern.edu>
2021-03-10 15:21:36 -0500

    init commit

---
+++ addl.js
@@ -0,0 +1,14 @@
+if (window.postMessage) {
+var tlMouseupFunc = function() {
+var tlFrame = document.getElementById("tl-timeline-iframe");
+if (tlFrame.contentWindow && tlFrame.contentWindow.postMessage) {
+tlFrame.contentWindow.postMessage("mouseup","*");
+}
+}
+if (typeof window.addEventListener != "undefined") {
+window.addEventListener("mouseup", tlMouseupFunc, false);
+}
+else if (typeof window.attachEvent != "undefined") {
+window.attachEvent("onmouseup", tlMouseupFunc);
+}
+}
